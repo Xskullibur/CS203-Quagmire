51920546f53a51b433f96ed2832c2e61efbd99da
ceewaigithub <chongyong321@gmail.com>
2024-08-22 16:41:21 +0800

    Added initial project files and configurations

---
+++ backend/src/main/java/com/project/G1_T3/G1T3Application.java
@@ -0,0 +1,12 @@
+package com.project.G1_T3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G1T3Application {
+	public static void main(String[] args) {
+		SpringApplication.run(G1T3Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/project/G1_T3/G1T3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.G1_T3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G1T3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
