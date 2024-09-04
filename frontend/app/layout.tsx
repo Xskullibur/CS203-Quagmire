fa5d8bdbabfaeee68b3edb3e71e1569ed3cbe66a
ceewaigithub <chongyong321@gmail.com>
2024-09-05 01:00:14 +0800

    Update layout.tsx to include favicon link tag

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-
+      <link rel="icon" href="/favicon.svg" />
       <body className={inter.className}>
         <MenuBar />
         {children}
