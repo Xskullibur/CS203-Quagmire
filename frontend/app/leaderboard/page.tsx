a0b3377ad81f20d7643cbde39e1d353bf26f581c
nibess1 <mohammed.nibras.2002@gmail.com>
2024-09-07 17:06:29 +0800

    <frontend> added leaderboard component

--- frontend/app/leaderboard/page.tsx
+++ frontend/app/leaderboard/page.tsx
@@ -1,26 +1,32 @@
 'use client'
 
 import React from 'react';
-import { Card, CardHeader, CardDescription, CardContent, CardTitle } from '@/components/ui/card';
+import { LeaderboardPosition } from '@/components/ui/leaderboardPosition';
+import {data} from '@/app/leaderboard/db.js';
+
+data.sort((a,b)=> {
+    if(a.score < b.score){
+        return -1;
+    } else {
+        return 1
+    }
+});
 
 export default function Leaderboard() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen">
             <h2 className="text-2xl font-bold mb-4">Leaderboard</h2>
-            <Card>
-                <CardHeader>
-                    <CardTitle className="text-2xl">Event Highlights</CardTitle>
-                    <CardDescription>What makes our tournament special</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <ul className="list-disc list-inside space-y-2">
-                        <li>International competitors from over 30 countries</li>
-                        <li>Live streaming of all matches</li>
-                        <li>Interactive fan experience with real-time voting</li>
-                        <li>Professional referees and state-of-the-art equipment</li>
-                    </ul>
-                </CardContent>
-            </Card>
+            <div className="w-4/5 h-auto bg-card">
+                <ul>
+                    <li>
+                        <LeaderboardPosition name="name" position="position" rating="rating"></LeaderboardPosition>
+                    </li>
+                    <li>
+            
+                    </li>
+                </ul>
+
+            </div>
         </div>
     );
 }
