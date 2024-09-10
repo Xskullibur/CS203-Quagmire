ab0e0068392bceb0e37f4118e7375e6447fdf81e
ceewaigithub <chongyong321@gmail.com>
2024-09-10 12:46:12 +0800

    Refactor dependencies in backend/pom.xml and add H2 database for testing

--- backend/src/main/java/com/project/G1_T3/authentication/controller/TestController.java
+++ backend/src/main/java/com/project/G1_T3/authentication/controller/TestController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import com.project.G1_T3.authentication.service.TestService;
 @RestController
 @RequestMapping("/test")
 @Profile("test")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class TestController {
     private static final Logger logger = LoggerFactory.getLogger(TestController.class);
 
