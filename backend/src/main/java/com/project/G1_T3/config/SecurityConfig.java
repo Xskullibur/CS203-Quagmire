f39e74f1301768f380694d92d4614910ab143bfe
Alson Sim <xskulliburdev@gmail.com>
2024-09-17 22:19:59 +0800

    refactor(authentication): move service calls to JwtService and extract PasswordEncoder
    - Moved service calls from AuthenticationController to JwtService to improve separation of concerns.
    - Extracted PasswordEncoder from SecurityConfig to remove cyclic dependency.

--- backend/src/main/java/com/project/G1_T3/config/SecurityConfig.java
+++ backend/src/main/java/com/project/G1_T3/config/SecurityConfig.java
@@ -14,7 +14,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -46,10 +45,8 @@ public class SecurityConfig {
     @Autowired
     private CustomUserDetailsService customUserDetailsService;
 
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(12);
-    }
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Bean
     AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
@@ -59,7 +56,7 @@ public class SecurityConfig {
     @Bean
     AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(passwordEncoder());
+        provider.setPasswordEncoder(passwordEncoder);
         provider.setUserDetailsService(customUserDetailsService);
         return provider;
     }
