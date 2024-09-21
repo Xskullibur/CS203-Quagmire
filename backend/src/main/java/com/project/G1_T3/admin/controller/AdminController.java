f40ca3d4dd06c67b5ecf2d51ea60486cfacb7a5d
Alson Sim <xskulliburdev@gmail.com>
2024-09-21 21:40:23 +0800

    feat(admin-registration): add register-admin endpoint
    - implement registerAdmin method
    - add AdminRegisterRequestDTO class
    - add spring-boot-starter-validation dependency

--- backend/src/main/java/com/project/G1_T3/admin/controller/AdminController.java
+++ backend/src/main/java/com/project/G1_T3/admin/controller/AdminController.java
@@ -3,6 +3,7 @@ package com.project.G1_T3.admin.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +11,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.G1_T3.admin.service.AdminService;
+import com.project.G1_T3.authentication.model.AdminRegisterRequestDTO;
 import com.project.G1_T3.user.model.UserDTO;
 import com.project.G1_T3.user.service.UserService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/admin")
@@ -20,6 +27,9 @@ public class AdminController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private AdminService adminService;
 
     @GetMapping("/dashboard")
     public ResponseEntity<String> getAdminDashboard() {
@@ -30,4 +40,15 @@ public class AdminController {
     public List<UserDTO> getAllUsers() {
         return userService.getAllUsers();
     }
+
+    @PostMapping("/register-admin")
+    public ResponseEntity<AdminRegisterRequestDTO> postMethodName(@Valid @RequestBody AdminRegisterRequestDTO registerRequest) {
+
+        adminService.registerAdmin(
+                registerRequest.getUsername(),
+                registerRequest.getEmail());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(registerRequest);
+    }
+
 }
--- backend/src/main/java/com/project/G1_T3/admin/service/AdminService.java
+++ backend/src/main/java/com/project/G1_T3/admin/service/AdminService.java
@@ -1,14 +1,39 @@
 package com.project.G1_T3.admin.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
+import com.project.G1_T3.authentication.service.PasswordGeneratorService;
+import com.project.G1_T3.user.model.UserRole;
+import com.project.G1_T3.user.service.UserService;
+
+import jakarta.transaction.Transactional;
+
 @Service
+@PreAuthorize("hasRole('ADMIN')")
 public class AdminService {
-    
-    @PreAuthorize("hasRole('ADMIN')")
+
+    private static final Logger logger = LoggerFactory.getLogger(AdminService.class);
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private PasswordGeneratorService passwordGeneratorService;
+
+    @Transactional
+    public void registerAdmin(String username, String email) {
+
+        String password = passwordGeneratorService.generatePassword();
+        userService.registerUser(username, email, password, UserRole.ADMIN);
+        logger.info("Admin registered successfully: {}:{}", username, password);
+    }
+
     public void getAdminDetails() {
-        
+
     }
 
 }
