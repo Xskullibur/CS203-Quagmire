f5ccbc7eba8de3871d6082976ba81c6d7bb4ecc3
ceewaigithub <chongyong321@gmail.com>
2024-10-07 18:54:07 +0800

    Refactor AWS Secrets Manager configuration in backend and frontend

--- backend/src/main/java/com/project/G1_T3/config/AwsSecretsConfig.java
+++ backend/src/main/java/com/project/G1_T3/config/AwsSecretsConfig.java
@@ -19,6 +19,7 @@ public class AwsSecretsConfig {
 
     @Bean
     public void setDatabaseSecret() throws Exception {
+
         AWSSecretsManager client = awsSecretsManager();
         String secretName = "rds!cluster-8210d2ac-0455-472b-9b71-6f9eca214f5f";
 
@@ -35,5 +36,6 @@ public class AwsSecretsConfig {
         System.setProperty("rds.username", secretJson.get("username").asText());
         System.setProperty("rds.password", secretJson.get("password").asText());
         System.setProperty("jwt.secret", secretJson.get("jwtSecret").asText());
+        
     }
 }
