51ec209b0a26c20aa5e8eeeb9342ead5108d66f8
Alson Sim <xskulliburdev@gmail.com>
2024-10-10 15:42:18 +0800

    refactor(auth): Update RegisterRequest and UserRepository

--- backend/src/main/java/com/project/G1_T3/authentication/model/RegisterRequest.java
+++ backend/src/main/java/com/project/G1_T3/authentication/model/RegisterRequest.java
@@ -18,14 +18,27 @@ public class RegisterRequest {
     @Size(min = 7, message = "Password must be at least 7 characters")
     private String password;
 
+    public RegisterRequest() {
+        super();
+    }
+
+    public RegisterRequest(
+            @NotBlank(message = "Username is mandatory") @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters") String username,
+            @NotBlank(message = "Email is mandatory") @Email(message = "Email should be valid") String email,
+            @NotBlank(message = "Password is mandatory") @Size(min = 7, message = "Password must be at least 7 characters") String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
     public String getUsername() {
         return username;
     }
-    
+
     public void setUsername(String username) {
         this.username = username;
     }
-    
+
     public String getEmail() {
         return email;
     }
