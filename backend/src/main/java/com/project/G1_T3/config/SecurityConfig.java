99a260c47514f1270d9d2efeabef07c75d4267d1
Alson Sim <xskulliburdev@gmail.com>
2024-10-15 14:38:58 +0800

    refactor(config): Update allowed headers in corsConfigurationSource

--- backend/src/main/java/com/project/G1_T3/config/SecurityConfig.java
+++ backend/src/main/java/com/project/G1_T3/config/SecurityConfig.java
@@ -104,11 +104,11 @@ public class SecurityConfig {
     }
 
     @Bean
-    CorsConfigurationSource corsConfigurationSource() {
+    public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
