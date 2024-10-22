48c7a59cc161d9bcb46048b7bbfa772919a5b613
Alson Sim <xskulliburdev@gmail.com>
2024-10-22 20:40:39 +0800

    refactor(authentication): set authentication when user logs in using username and password

---
+++ backend/src/main/java/com/project/G1_T3/security/service/SecurityService.java
@@ -0,0 +1,18 @@
+package com.project.G1_T3.security.service;
+
+import com.project.G1_T3.user.model.CustomUserDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityService {
+
+    public CustomUserDetails getAuthenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof CustomUserDetails) {
+            return (CustomUserDetails) authentication.getPrincipal();
+        }
+        return null;
+    }
+}
