f786e5ff0b6b88a2f7069d1c9021d6467128d2db
Alson Sim <xskulliburdev@gmail.com>
2024-10-24 19:00:56 +0800

    feat(fs-service): Added backend functionality to upload images to Firebase

---
+++ backend/src/main/java/com/project/G1_T3/filestorage/controller/FileController.java
@@ -0,0 +1,28 @@
+package com.project.G1_T3.filestorage.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.project.G1_T3.filestorage.service.FileStorageService;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/files")
+public class FileController {
+
+    @Autowired
+    private FileStorageService fileStorageService;
+
+    @PostMapping()
+    public ResponseEntity<String> uploadFile(@RequestBody MultipartFile file) {
+        try {
+            String fileUrl = fileStorageService.uploadFile("profile-picture", file.getOriginalFilename(), file);
+            return ResponseEntity.ok(fileUrl);
+        } catch (IOException e) {
+            return ResponseEntity.status(500).body("File upload failed: " + e.getMessage());
+        }
+    }
+}
